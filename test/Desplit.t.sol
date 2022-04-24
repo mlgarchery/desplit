7c06a42348c6fb302456b2632b7ccb3815bc1504
mlgarchery <martial.garchery@gmail.com>
2022-04-24 11:11:34 +0200

    Use forge-std/Test.sol

--- test/Desplit.t.sol
+++ test/Desplit.t.sol
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "ds-test/test.sol";
+import "forge-std/Test.sol";
 import "forge-std/console2.sol";
 import "src/Desplit.sol";
 
 
-contract DesplitTest is DSTest {
+contract DesplitTest is Test {
     Desplit c;
-    function setUp() public {
+    function setUp() public logs_gas {
         c = new Desplit();
+        // console2.log(address(this));
+        // emit log_address(address(this));
     }
 
-    function testConsoleLog() public {
-        console2.log("test");
+    function testConsoleLog() public logs_gas {
         assertTrue(true);
     }
 }
